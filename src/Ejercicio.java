3852c07d2e1f0aa43b4dac31d4738d3783644ec2
Administrador <Administrador@PC>
2017-05-02 12:37:14 +0200

    Primera version con git

---
+++ src/Ejercicio.java
@@ -0,0 +1,16 @@
+
+public class Ejercicio {
+
+	static int n1=2;
+	static int n2=4;
+	static int n3;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("El resultado de la suma es: "+(n3=n1+n2));
+		System.out.println("n3 tiene ahora el resultado de la suma");
+		System.out.println("Lo comprobamos ---> "+n3);
+	}
+}
